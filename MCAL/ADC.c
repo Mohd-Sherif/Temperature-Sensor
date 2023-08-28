84fb2b74bf66699d27b6823864dd764324440003
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-29 00:54:04 +0300

    Create ADC.c

---
+++ MCAL/ADC.c
@@ -0,0 +1,33 @@
+/*
+ * ADC.c
+ *
+ * Created: 04-Jul-22 1:42:05 PM
+ *  Author: Mohamed Sherif
+ */ 
+
+#include <avr/io.h>
+#include "../LIB/std_macros.h"
+
+void ADC_vinit(void){
+	SET_BIT(ADMUX,REFS0); //configure VREF
+	SET_BIT(ADMUX,REFS1); //VREF=2.56V******
+	SET_BIT(ADMUX,MUX4); //(AD0-AD1)*1
+	SET_BIT(ADCSRA,ADEN); //enable ADC
+	/*adjust ADC clock*/
+	SET_BIT(ADCSRA,ADPS2);
+	SET_BIT(ADCSRA,ADPS1);
+}
+
+unsigned short ADC_u16read(void){
+	unsigned short read_val;
+	SET_BIT(ADCSRA,ADSC);
+	while(READ_BIT(ADCSRA,ADSC)); //wait until the conversion be done
+	//another method for the waiting the conversion
+	/*
+	while(IS_BIT_CLR(ADCSRA,ADIF)); //stay in your position till ADIF become 1
+	SET_BIT(ADCSRA,ADIF); //clear ADIF
+	*/
+	read_val=ADCL;
+	read_val|=(ADCH<<8);
+	return read_val;
+}
